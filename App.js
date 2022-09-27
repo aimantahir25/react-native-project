6aeae52fac1ffc7d84712b56a2dd272585b7f36e
aimantahir25 <113507237+aimantahir25@users.noreply.github.com>
2022-09-27 12:28:04 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    setCourseGoals([...courseGoals, enteredGoalText]);
+  }
+  const renderGoals = () => {
+    if (!courseGoals) return null;
+    return courseGoals.map((goal) => {
+      return (
+        <View style={goalItem} key={goal}>
+          <Text style={goalText}>{goal}</Text>
+        </View>
+      )
+    }
+    )
+  }
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Write your goals..."
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler}></Button>
+      </View>
+      <View style={styles.goalsContainer}>
+        {renderGoals}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomColor: "#cccccc",
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
+  },
+  goalText: {
+    color: "white",
+  },
+});
